f67df4e992dae52ca7527305662587efc74485f3
Murshid9873 <murshid9873@gmail.com>
2022-10-03 12:30:11 +0530

    change

--- schoolerp/settings.py
+++ schoolerp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-
+    'school_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- schoolerp/urls.py
+++ schoolerp/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('school_admin/', include('school_admin.urls'))
+    path('', include('school_admin.urls'))
 
 ]
