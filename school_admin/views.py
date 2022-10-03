f93e0d89f89cb307d3c4200ab71df88b30cb2a8f
Murshid9873 <murshid9873@gmail.com>
2022-10-03 12:35:40 +0530

    change

--- school_admin/views.py
+++ school_admin/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-     return render(request,'schooltemplates/home.html'),
+     return render(request,'schooltemplates/home.html')
+     
 def viewstaff(request):
      return render(request,'schooltemplates/viewstaff.html')     
