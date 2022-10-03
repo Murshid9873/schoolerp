1c72bc1d600151984346cea05825ecea789ab646
Murshid9873 <murshid9873@gmail.com>
2022-10-03 12:23:04 +0530

    change

--- schoolerp/settings.py
+++ schoolerp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+1wht&vg)dh=1bqueld8n5g^7(sqtxs((7lu^le*o5@v$!=lvp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
